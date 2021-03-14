a0c9ba3482d7fd547a259c2e55a109b0de8c2ac3
qmarr <komarenetsv@gmail.com>
2021-03-14 12:19:17 +0200

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+  let arr = String(number).split('');
+  let sNumbers = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  let tNumbers = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  let thNumbers = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  let str = '';
+  if(arr.length == 1)
+    return sNumbers[arr[0]];
+  else if (arr.length == 2) {
+    if(arr[0] == 1)
+      return tNumbers[arr[1]];
+    else {
+        if(arr[1] == 0)
+          return thNumbers[arr[0]-2];
+        else
+          return `${thNumbers[arr[0]-2]} ${sNumbers[arr[1]]}`;
+    }
+  } else {
+    if(arr[1] == 0 && arr[2] == 0)
+      return `${sNumbers[arr[0]]} hundred`
+    if(arr[1] == 1)
+  	  return `${sNumbers[arr[0]]} hundred ${tNumbers[arr[2]]}`
+    if(arr[1] == 0)
+      return `${sNumbers[arr[0]]} hundred ${sNumbers[arr[2]]}`
+    if(arr[2] == 0)
+      return `${sNumbers[arr[0]]} hundred ${thNumbers[arr[1]-2]}`
+    return `${sNumbers[arr[0]]} hundred ${thNumbers[arr[1]-2]} ${sNumbers[arr[2]]}`
+  }
 }
